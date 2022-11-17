3d04923242089ba09479afbe477dac0aa136e675
doldrums <doldrums.317@gmail.com>
2022-11-17 14:00:19 +0300

    ios part

---
+++ ios/Classes/DataReceivedHandler.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public class DataReceivedHandler: NSObject, FlutterStreamHandler {
+
+   private var eventSink: FlutterEventSink?
+
+   fileprivate func register(with registrar: FlutterPluginRegistrar, peripheral: CBPeripheralManager) {
+
+       let eventChannel = FlutterEventChannel(name: "dev.steenbakker.flutter_ble_peripheral/ble_data_received",
+                                              binaryMessenger: registrar.messenger())
+       eventChannel.setStreamHandler(self)
+
+       peripheral.onDataReceived = { data in
+           if let eventSink = self.eventSink {
+               eventSink(FlutterStandardTypedData(bytes: data))
+           }
+       }
+   }
+
+   public func onListen(withArguments arguments: Any?,
+                        eventSink: @escaping FlutterEventSink) -> FlutterError? {
+       self.eventSink = eventSink
+       return nil
+   }
+
+   public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+       eventSink = nil
+       return nil
+   }
+}
--- ios/Classes/SwiftBleReaderPlugin.swift
+++ ios/Classes/SwiftBleReaderPlugin.swift
@@ -1,14 +1,36 @@
 import Flutter
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
 public class SwiftBleReaderPlugin: NSObject, FlutterPlugin {
+
+  private lazy var flutterBlePeripheralManager: CBPeripheralManager  = CBPeripheralManager(delegate: self, queue: nil)
+  private let dataReceivedHandler = DataReceivedHandler()
+
+  init(stateChangedHandler: StateChangedHandler) {
+        self.stateChangedHandler = stateChangedHandler
+        flutterBlePeripheralManager = FlutterBlePeripheralManager(stateChangedHandler: stateChangedHandler)
+        super.init()
+  }
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "ble_reader", binaryMessenger: registrar.messenger())
-    let instance = SwiftBleReaderPlugin()
+    let instance = SwiftBleReaderPlugin(stateChangedHandler: StateChangedHandler(registrar: registrar))
     registrar.addMethodCallDelegate(instance, channel: channel)
+
+    // Event channel
+    instance.dataReceivedHandler.register(with: registrar, peripheral: instance.flutterBlePeripheralManager)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch (call.method) {
+        case "start":
+            result(FlutterMethodNotImplemented)
+        case "stop":
+            result(FlutterMethodNotImplemented)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
   }
 }
